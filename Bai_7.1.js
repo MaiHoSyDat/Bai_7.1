141da6ab592b28dac7b1593f263ff8feb9f58fd1
MaiHoSyDat <maihosydat@gmail.com>
2023-04-05 14:41:36 +0700

    Bai_7.1

---
+++ Bai_7.1.js
@@ -0,0 +1,24 @@
+// let i= 10;
+// let f = 20.5 ;
+// let z = true;
+// let s = 'Hà Nội'
+// let weight = 15;
+// let height = 11;
+// let area = weight * height
+
+let a = prompt("nhập số a");
+let b = prompt("nhập số b");
+let c = a % b == 0;
+
+
+// document.write('i = '+ i);
+// document.write('<br/>');
+// document.write('f = ' +f);
+// document.write('<br/>');
+// document.write('b = '+ z);
+// document.write('<br/>');
+// document.write('s = '+ s);
+// document.write('<br/>')
+// document.write('area = ' + area)
+// document.write('<br/>')
+alert( "a là bội của b " + c )
